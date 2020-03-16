5a11f12a77c3f5aefcd345c7edb21818e5b785e6
Marcin Lawicki <m4rcin@tutanota.com>
2020-03-16 16:22:59 +0100

    Adjust to change in endpoint data format

--- app/src/main/java/com/mobilecoronatracker/model/pojo/CovidCumulatedData.kt
+++ app/src/main/java/com/mobilecoronatracker/model/pojo/CovidCumulatedData.kt
@@ -1,3 +1,4 @@
 package com.mobilecoronatracker.model.pojo
 
-data class CovidCumulatedData(val cases: Int = 0, val deaths: Int = 0, val recovered: Int = 0)
+data class CovidCumulatedData(val cases: Int = 0, val deaths: Int = 0, val recovered: Int = 0,
+                              val updated: Long = 0)

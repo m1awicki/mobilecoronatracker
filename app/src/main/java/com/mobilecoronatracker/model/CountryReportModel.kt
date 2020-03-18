5c13e972dc32996016d95bc1c65b28072c422160
Mikołaj Demków <mdemkow@gmail.com>
2020-03-18 22:40:26 +0100

    Add flagged and more data indicators, change to immutable model

--- app/src/main/java/com/mobilecoronatracker/model/CountryReportModel.kt
+++ app/src/main/java/com/mobilecoronatracker/model/CountryReportModel.kt
@@ -1,18 +1,32 @@
 package com.mobilecoronatracker.model
 
+import androidx.lifecycle.MutableLiveData
 import com.mobilecoronatracker.model.pojo.CovidCountryEntry
+import java.util.Random
 
-class CountryReportModel(
-    override var country: String,
-    override var cases: Int,
-    override var todayCases: Int,
-    override var deaths: Int,
-    override var todayDeaths: Int,
-    override var recovered: Int,
-    override var critical: Int,
-    override var active: Int
+data class CountryReportModel(
+    override val country: String,
+    override val cases: Int,
+    override val todayCases: Int,
+    override val deaths: Int,
+    override val todayDeaths: Int,
+    override val recovered: Int,
+    override val critical: Int,
+    override var active: Int,
+    override val flagged: MutableLiveData<Boolean>,
+    override val hasMoreData: Boolean
 ) : CountryReportModelable {
     constructor(data: CovidCountryEntry) :
-            this(data.country, data.cases, data.todayCases, data.deaths, data.todayDeaths,
-                data.recovered, data.critical, data.active)
+            this(
+                data.country,
+                data.cases,
+                data.todayCases,
+                data.deaths,
+                data.todayDeaths,
+                data.recovered,
+                data.critical,
+				data.active,
+                MutableLiveData(Random().nextDouble() > 0.5),
+                Random().nextDouble() > 0.5
+            )
 }
--- app/src/main/java/com/mobilecoronatracker/model/CountryReportModelable.kt
+++ app/src/main/java/com/mobilecoronatracker/model/CountryReportModelable.kt
@@ -1,12 +1,16 @@
 package com.mobilecoronatracker.model
 
+import androidx.lifecycle.LiveData
+
 interface CountryReportModelable {
-    var country: String
-    var cases: Int
-    var todayCases: Int
-    var deaths: Int
-    var todayDeaths: Int
-    var recovered: Int
-    var critical: Int
-    var active: Int
+    val country: String
+    val cases: Int
+    val todayCases: Int
+    val deaths: Int
+    val todayDeaths: Int
+    val recovered: Int
+    val critical: Int
+	val active: Int
+    val flagged: LiveData<Boolean>
+    val hasMoreData: Boolean
 }

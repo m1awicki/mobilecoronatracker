5664ed47ad8261db7d1be6756fd5507fdb6977f4
Mikołaj Demków <mdemkow@gmail.com>
2020-03-28 11:11:22 +0100

    Make BaseDao suspend

--- app/src/main/java/com/mobilecoronatracker/data/persistence/dao/BaseDao.kt
+++ app/src/main/java/com/mobilecoronatracker/data/persistence/dao/BaseDao.kt
@@ -2,18 +2,19 @@ package com.mobilecoronatracker.data.persistence.dao
 
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Update
 
 interface BaseDao<T> {
-    @Insert
-    fun insert(data: T)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(data: T): Long
 
     @Insert
-    fun insert(vararg data: T)
+    suspend fun insert(vararg data: T)
 
     @Update
-    fun update(vararg data: T)
+    suspend fun update(vararg data: T)
 
     @Delete
-    fun delete(vararg data: T)
+    suspend fun delete(vararg data: T)
 }

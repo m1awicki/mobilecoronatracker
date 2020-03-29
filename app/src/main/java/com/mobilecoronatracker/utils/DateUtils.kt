4bd9c8999b75b25cf8302cdeac6bc6ee58eb1124
Mikołaj Demków <mdemkow@gmail.com>
2020-03-29 18:30:43 +0200

    Changed getTodayTimestamp due to reviewer brilliant catch and idea

--- app/src/main/java/com/mobilecoronatracker/utils/DateUtils.kt
+++ app/src/main/java/com/mobilecoronatracker/utils/DateUtils.kt
@@ -1,15 +1,5 @@
 package com.mobilecoronatracker.utils
 
-import java.util.Calendar
-import java.util.TimeZone
+fun getTodayTimestamp(): Long = (System.currentTimeMillis() / MILLIS_PER_DAY) * MILLIS_PER_DAY
 
-fun getTodayTimestamp(): Long {
-    val currentTimeMillis = System.currentTimeMillis()
-    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-    calendar.timeInMillis = currentTimeMillis
-    calendar.set(Calendar.HOUR_OF_DAY, 0)
-    calendar.set(Calendar.MINUTE, 0)
-    calendar.set(Calendar.SECOND, 0)
-    calendar.set(Calendar.MILLISECOND, 0)
-    return calendar.timeInMillis
-}
+private const val MILLIS_PER_DAY = 1000 * 60 * 60 * 24

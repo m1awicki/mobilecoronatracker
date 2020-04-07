bce11297760f3ff55306ae6198c71635854413b7
Marcin Lawicki <m4rcin@tutanota.com>
2020-04-08 00:13:31 +0200

    Add new properties to view model
    Cases, deaths and recovered values are separated from labels.
    Added new properties for historical data.

--- app/src/main/java/com/mobilecoronatracker/ui/cumulatedreport/CumulatedReportViewModelable.kt
+++ app/src/main/java/com/mobilecoronatracker/ui/cumulatedreport/CumulatedReportViewModelable.kt
@@ -8,6 +8,17 @@ interface CumulatedReportViewModelable {
     val active: LiveData<String>
     val deaths: LiveData<String>
     val recovered: LiveData<String>
+    var casesCount: Int
+    var activeCount: Int
+    var deathsCount: Int
+    var recoveredCount: Int
+    val todayUpdated: LiveData<Void>
+    val casesHistory: LiveData<List<Int>>
+    val deathsHistory: LiveData<List<Int>>
+    val recoveredHistory: LiveData<List<Int>>
+    var history: List<List<Float>>
+    var historyLabels: List<String>
+    val historyUpdated: LiveData<Void>
     val isRefreshing: MutableLiveData<Boolean>
     fun onRefreshRequested()
 }

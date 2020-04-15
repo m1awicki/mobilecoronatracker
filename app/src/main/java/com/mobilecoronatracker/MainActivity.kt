f588a5c4e095b4bee39a1a3ea3f836d24551f1cb
Mikołaj Demków <mdemkow@gmail.com>
2020-04-15 22:39:09 +0200

    Add toolbar to main activity

--- app/src/main/java/com/mobilecoronatracker/MainActivity.kt
+++ app/src/main/java/com/mobilecoronatracker/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.mobilecoronatracker
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +18,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_per_country, R.id.navigation_cumulated, R.id.navigation_about))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_per_country,
+                R.id.navigation_cumulated,
+                R.id.navigation_about
+            )
+        )
+        toolbar.setupWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }

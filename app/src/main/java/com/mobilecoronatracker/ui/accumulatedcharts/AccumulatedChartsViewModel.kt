1c5af309e66be56fdc6ee1384a4e4ce3c7766030
Marcin Lawicki <m4rcin@tutanota.com>
2020-04-17 23:52:27 +0200

    Add more information to accumulated details screen
    Added two donut charts: tested-to-infected and critical-to-active.
    Added information about total numbers of infected, dead and recovered.
    Added today's increses info and count of affected countries.

--- app/src/main/java/com/mobilecoronatracker/ui/accumulatedcharts/AccumulatedChartsViewModel.kt
+++ app/src/main/java/com/mobilecoronatracker/ui/accumulatedcharts/AccumulatedChartsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mobilecoronatracker.data.repository.AccumulatedDataRepo
+import com.mobilecoronatracker.model.GeneralReportModelable
 import com.mobilecoronatracker.model.GeneralReportTimePointModelable
 import com.mobilecoronatracker.ui.accumulatedcharts.AccumulatedChartsViewModelable.HistoryChartData
 import com.mobilecoronatracker.utils.asSimpleDate
@@ -16,6 +17,8 @@ class AccumulatedChartsViewModel(
 ) : ViewModel(), AccumulatedChartsViewModelable {
     override val historyChartUpdate = MutableLiveData<HistoryChartData>()
     override val activeCasesChartUpdate = MutableLiveData<HistoryChartData>()
+    override val affectedCountries = MutableLiveData<Int>()
+    override val globalDataWithToday = MutableLiveData<AccumulatedChartsViewModelable.GlobalDataWithToday>()
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
@@ -23,6 +26,24 @@ class AccumulatedChartsViewModel(
                 onAccumulatedHistory(it)
             }
         }
+        viewModelScope.launch(Dispatchers.IO) {
+            accumulatedDataRepo.getDataForLastDays(2).collect {
+                onRecentDays(it)
+            }
+        }
+    }
+
+    private fun onRecentDays(data: List<GeneralReportModelable>) {
+        val today = data.last()
+        val yesterday = data.first()
+        affectedCountries.postValue(today.affectedCountries)
+        globalDataWithToday.postValue(
+            AccumulatedChartsViewModelable.GlobalDataWithToday(
+                today.cases, today.todayCases, today.deaths, today.todayDeaths,
+                today.cases - today.deaths - today.recovered, today.recovered,
+                today.recovered - yesterday.recovered, today.critical, today.tests
+            )
+        )
     }
 
     private fun onAccumulatedHistory(data: List<GeneralReportTimePointModelable>) {
@@ -44,7 +65,6 @@ class AccumulatedChartsViewModel(
                 labels
             )
         )
-
         activeCasesChartUpdate.postValue(
             HistoryChartData(
                 listOf(active),
